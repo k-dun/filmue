fd0c70b2c6502105dccc3d54d15a7bf569cb331d
Konrad Dunski <44006045+k-dun@users.noreply.github.com>
2024-06-04 23:13:58 +0100

    API fix.

--- src/app/api/omdbApi.ts
+++ src/app/api/omdbApi.ts
@@ -3,6 +3,6 @@ import axios from 'axios';
 const OMDB_API_KEY = '79740b73' || '';
 
 export const fetchMovieDetails = async (title: string) => {
-  const response = await axios.get(`http://www.omdbapi.com/?apikey=${OMDB_API_KEY}&t=${encodeURIComponent(title)}`);
+  const response = await axios.get(`https://www.omdbapi.com/?apikey=${OMDB_API_KEY}&t=${encodeURIComponent(title)}`);
   return response.data;
 };
