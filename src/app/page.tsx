cb6e55a83dffa119dffc17dfc819324d60d260eb
Konrad Dunski <44006045+k-dun@users.noreply.github.com>
2024-06-04 03:08:33 +0100

    All basic functionality up and running.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,102 @@
 "use client";
 
-import Image from "next/image";
-import Link from "next/link";
+import { useState } from 'react';
+import { movieTitles } from './data/movies';
+import { fetchMovieDetails } from './api/omdbApi';
+import Image from 'next/image';
+import Link from 'next/link';
 
 export default function Home() {
+  const [currentMovie, setCurrentMovie] = useState('');
+  const [currentClue, setCurrentClue] = useState(0);
+  const [clues, setClues] = useState<string[]>([]);
+  const [userInput, setUserInput] = useState('');
+  const [startedGame, setStartedGame] = useState(false);
+
+  const startGame = () => {
+    const randomIndex = Math.floor(Math.random() * movieTitles.length);
+    const randomMovie = movieTitles[randomIndex];
+    setCurrentMovie(randomMovie);
+    fetchMovieDetails(randomMovie)
+      .then((data) => {
+        const cluesArray = generateClues(data);
+        setClues(cluesArray);
+        setStartedGame(true);
+      })
+      .catch((error) => console.error(error));
+  };
+
+  const endGame = () => {
+    setStartedGame(false);
+  }
+
+  const generateClues = (movieData: any): string[] => {
+    const cluesArray: string[] = [];
+    const actors = movieData.Actors.split(', ');
+    cluesArray.push(`Starring: ${actors[0]}`);
+    cluesArray.push(`Released: ${movieData.Released} | Genre: ${movieData.Genre}`);
+    cluesArray.push(`Starring: ${actors[1]}`);
+    cluesArray.push(`Director: ${movieData.Director}`);
+    if (movieData.Awards) {
+      cluesArray.push(`Awards: ${movieData.Awards}`);
+    } else {
+      cluesArray.push(`Starring: ${actors[2]}`);
+    }
+    cluesArray.push(`Plot: ${movieData.Plot}`);
+    return cluesArray;
+  };
+
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setUserInput(e.target.value);
+  };
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    if (userInput.toLowerCase() === currentMovie.toLowerCase()) {
+      alert(`Well done! You guessed correctly on the ${currentClue + 1} try!`);
+      setCurrentClue(0);
+      setClues([]);
+      setUserInput('');
+      endGame();
+    } else {
+      if (currentClue === clues.length - 1) {
+        alert('Sorry, you have used up all the clues. The movie was ' + currentMovie);
+        setCurrentClue(0);
+        setClues([]);
+        setUserInput('');
+        endGame();
+      } else {
+        setCurrentClue(currentClue + 1);
+        setUserInput('');
+      }
+    }
+  };
+
   return (
-    <div className="flex flex-col items-center justify-center h-[100vh] w-[100vw]">
-      <Image src="/filmue-logo.png" className="mb-12" alt="Filmue Logo" width={400} height={100} />
-      <Link href="/quiz" className="bg-[#202020] hover:bg-[#404040] text-[#FCFAFF] font-bold py-3 px-10 shadow hover:shadow-xl rounded-lg">
-        Start!
-      </Link>
-    </div>
+    <>
+      <div className="flex flex-col items-center justify-center h-[100vh] w-[100vw]">
+        <Link href="/">
+          <Image src="/filmue-logo.png" priority={true} className="mb-12" alt="Filmue Logo" width={400} height={100} />
+        </Link>
+        {!startedGame && (
+          <button onClick={startGame} className="bg-[#202020] hover:bg-[#404040] text-[#FCFAFF] font-bold py-3 px-10 shadow hover:shadow-xl rounded-lg mb-5">
+            Let's go!
+          </button>
+        )}
+        {clues.slice(0, currentClue + 1).map((clue, index) => (
+          <div key={index} className="">
+            <p className="flex flex-wrap w-[30vw] justify-center items-center border-2 bg-[#808080] text-[#FCFAFF] rounded-lg px-6 py-4 mb-3">
+              {clue}
+            </p>
+          </div>
+        ))}
+        {startedGame && (
+          <form onSubmit={handleSubmit}>
+            <input type="text" value={userInput} placeholder="Movie title" onChange={handleInputChange} className="border border-[#202020] py-3 px-2 rounded-lg mr-2" />
+            <button type="submit" className="bg-[#202020] hover:bg-[#404040] text-[#FCFAFF] font-bold py-3 px-10 shadow hover:shadow-xl rounded-lg">Submit..</button>
+          </form>
+        )}
+      </div>
+    </>
   );
 }
